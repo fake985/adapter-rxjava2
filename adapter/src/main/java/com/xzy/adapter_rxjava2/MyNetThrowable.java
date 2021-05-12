3934c509833dfb1ccdd9de68cf5aabb4c681bdad
ning <caichunning@essex.com.cn>
2021-05-12 10:46:25 +0800

    2021-5-12

--- adapter/src/main/java/com/xzy/adapter_rxjava2/MyNetThrowable.java
+++ adapter/src/main/java/com/xzy/adapter_rxjava2/MyNetThrowable.java
@@ -2,10 +2,6 @@ package com.xzy.adapter_rxjava2;
 
 import retrofit2.Call;
 
-/**
- * @author: carbon dioxide
- * @time: 2021/4/25 16:24
- */
 public class MyNetThrowable extends Throwable {
 
     public final Call call;
